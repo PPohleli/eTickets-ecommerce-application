93564be540456f89faf0d3a9d71b87aaff077370
Pohleli_Phiwe <pohleliphiwe@gmail.com>
2023-10-22 10:34:10 +0200

    Added a new folder Base in the Data folder, and created IEntityBase, IEntityBaseRepository with its method signitures, and EntityBaseRepository to inherit from IEntityBaseRepository.

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Actor> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Actor> UpdateAsync(int id, T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
