b1400b8b60b4bafb7328eccbbb06659ab3d074b9
Pohleli_Phiwe <pohleliphiwe@gmail.com>
2023-10-27 23:21:53 +0200

    Adding new cinema to the database.

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -19,6 +20,20 @@ namespace eTickets.Controllers
         {
             var allCinemas = await _sevice.GetAllAsync();
             return View(allCinemas);
+        }
+
+        //Get: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo, Name, Description")]Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _sevice.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -8,11 +8,18 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
         public string Logo { get; set; }
+
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 chars")]
         public string Name { get; set; }
+
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         //Relatioships
