3167e2127b81479013966d75c925338465caf416
Pohleli_Phiwe <pohleliphiwe@gmail.com>
2023-11-06 15:38:51 +0200

    Populate dropdown input fields with respective data in the Create movie view.

--- eTickets/Data/Services/MoviesService.cs
+++ eTickets/Data/Services/MoviesService.cs
@@ -1,6 +1,8 @@
 ﻿using eTickets.Data.Base;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
@@ -22,6 +24,23 @@ namespace eTickets.Data.Services
                 .FirstOrDefaultAsync(n => n.Id == id);
 
             return movieDetails;
+        }
+
+        public async Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues()
+        {
+            //var theResponce = new NewMovieDropdownsVM();
+
+            //theResponce.Actors = await _context.Actors.OrderBy(ac => ac.FullName).ToListAsync();
+            //theResponce.Cinemas = await _context.Cinemas.OrderBy(c => c.Name).ToListAsync();
+            //theResponce.Producers = await _context.Producers.OrderBy(p => p.FullName).ToListAsync();
+
+            var theResponce = new NewMovieDropdownsVM()
+            {
+                Actors = await _context.Actors.OrderBy(ac => ac.FullName).ToListAsync(),
+                Cinemas = await _context.Cinemas.OrderBy(c => c.Name).ToListAsync(),
+                Producers = await _context.Producers.OrderBy(p => p.FullName).ToListAsync()
+            };
+            return theResponce;
         }
     }
 }
