262124fc8eb461be4f966272c8d09835af51b539
Pohleli_Phiwe <pohleliphiwe@gmail.com>
2023-11-11 11:05:22 +0200

    Setting up ShoppingCart class: ShoppingCart , AppDbContext, Migration - ShoppingCartItems_updated (to add Movie property).

---
+++ eTickets/Migrations/20231111085435_ShoppingCartItems_updated.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class ShoppingCartItems_updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MovieId",
+                table: "ShoppingCartItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_MovieId",
+                table: "ShoppingCartItems",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Movies_MovieId",
+                table: "ShoppingCartItems",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Movies_MovieId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCartItems_MovieId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "MovieId",
+                table: "ShoppingCartItems");
+        }
+    }
+}
