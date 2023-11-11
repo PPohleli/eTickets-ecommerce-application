87ca4ea27bb34437a5add4d993b549d41f86746e
Pohleli_Phiwe <pohleliphiwe@gmail.com>
2023-11-11 19:34:29 +0200

    Added a viewComponent to display number of distinct cart items: ShoppingCartSummery, Default , _layout.

---
+++ eTickets/Data/ViewComponents/ShoppingCartSummery.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummery : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummery(ShoppingCart shoppingCart) 
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cartItems = _shoppingCart.GetShoppingCartItems();
+
+            return View(cartItems.Count);
+        }
+    }
+}
