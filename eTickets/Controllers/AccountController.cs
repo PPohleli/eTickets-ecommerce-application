890ca372df0960d0a4f29668a4fff862010a4bda
Pohleli_Phiwe <pohleliphiwe@gmail.com>
2023-11-16 18:25:53 +0200

    Signing out users.

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -75,5 +75,11 @@ namespace eTickets.Controllers
 
             return View("RegisterCompleted");
         }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
